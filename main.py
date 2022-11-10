797794f6207952f8826d98980d77abec6e7f92a7
matolgithub <89175955650@mail.ru>
2022-11-10 15:22:20 +0300

    Next commit to Git.

--- main.py
+++ main.py
@@ -7,21 +7,43 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+url = f"https://api.telegram.org/bot{getenv('MY_TOKEN')}"
 my_tlg_bot_091122 = telebot.TeleBot(getenv("MY_TOKEN"))
 headers = Headers()
 header = headers.generate()
 
 
 def check_bot():
-    url = f"https://api.telegram.org/bot{getenv('MY_TOKEN')}"
     check_method = "/getMe"
     response = requests.get(url=f"{url}{check_method}", headers=header)
     print(response, response.status_code)
-    pprint(response.text)
+    print(response.text)
+    pprint(response.json())
+    print(f'ID-bot: {response.json()["result"]["id"]}\nUsername: {response.json()["result"]["username"]}')
+
+
+def send_msg_tothebot():
+    send_msg_method = "/sendMessage"
+    response = requests.get(url=f"{url}{send_msg_method}?chat_id={getenv('CHAT_ID')}&text={getenv('TEXT')}",
+                            headers=header)
+    pprint(response.json())
+
+    return response.json()
+
+
+def get_updates():
+    get_updates_method = "/getUpdates"
+    response = requests.get(url=f"{url}{get_updates_method}", headers=header)
+    print(response, response.status_code)
+    pprint(response.json())
+
+    return response.json()
 
 
 def main():
-    check_bot()
+    # check_bot()
+    # send_msg_tothebot()
+    get_updates()
 
 
 if __name__ == "__main__":
